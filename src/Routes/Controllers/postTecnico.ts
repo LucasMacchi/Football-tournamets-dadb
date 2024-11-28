ec0c61d978a658d2e111f6157715e4a2afae1eff
LucasMacchi <lucasmacchi25@gmail.com>
2024-11-28 19:18:30 -0300

    telefono a string

--- src/Routes/Controllers/postTecnico.ts
+++ src/Routes/Controllers/postTecnico.ts
@@ -1,6 +1,6 @@
 import QueryMaker from "../Utils/queryMaker";
 
-export default async function postTenico (nombre: string, apellido: string, direccion: string, telefono: number, nacimiento: Date) {
+export default async function postTenico (nombre: string, apellido: string, direccion: string, telefono: string, nacimiento: Date) {
     const nac = new Date(nacimiento)
     const sql = `insert into public.tecnico(nombre,apellido,direccion,telefono,nacimiento) 
     values ('${nombre}', '${apellido}', '${direccion}', '${telefono}','${nac.toDateString()}') RETURNING tecnico_pk`
