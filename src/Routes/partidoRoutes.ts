5974742eacaf438e8277536389effbc27d8a214c
LucasMacchi <lucasmacchi25@gmail.com>
2024-11-28 12:37:50 -0300

    trycatchs

--- src/Routes/partidoRoutes.ts
+++ src/Routes/partidoRoutes.ts
@@ -12,59 +12,119 @@ import getAll from "./Utils/getAll";
 //Rutas
 //Crear Gol
 partido_routes.post("/gol", async (req:IRequest, res: IResponse) => {
-    const {tiempo, asistencia, goleador, encuentro} = req.body
-    await postGol(tiempo, asistencia, goleador, encuentro);
-    res.send("Gol anotado.")
+    try {
+        const {tiempo, asistencia, goleador, encuentro} = req.body
+        await postGol(tiempo, asistencia, goleador, encuentro);
+        res.send("Gol anotado.")
+    } catch (error) {
+        if(error instanceof Error) res.status(404).send(error.message)
+        else res.status(404).send("Error")
+    }
+
 });
 //Crear Tarjeta
 partido_routes.post("/tarjeta", async (req:IRequest, res: IResponse) => {
-    const {tiempo, tipo_tarjeta, jugador, encuentro} = req.body
-    await postTarjeta(tiempo, tipo_tarjeta, jugador, encuentro);
-    res.send("Tarjeta creada.")
+    try {
+        const {tiempo, tipo_tarjeta, jugador, encuentro} = req.body
+        await postTarjeta(tiempo, tipo_tarjeta, jugador, encuentro);
+        res.send("Tarjeta creada.")
+    } catch (error) {
+        if(error instanceof Error) res.status(404).send(error.message)
+        else res.status(404).send("Error")
+    }
+
 });
 //--------------------------GETTER--------------------------//
 //Obtener los goles de un encuentro
 partido_routes.get("/goles/:id", async (req: IRequest, res: IResponse) => {
-    const id = req.params.id
-    const response = await getById("gol", "encuentro", parseInt(id))
-    res.send(response)
+    try {
+        const id = req.params.id
+        const response = await getById("gol", "encuentro", parseInt(id))
+        res.send(response)
+    } catch (error) {
+        if(error instanceof Error) res.status(404).send(error.message)
+        else res.status(404).send("Error")
+    }
+
 });
 //Obtener los goles de un encuentro
 partido_routes.get("/tarjetas/:id", async (req: IRequest, res: IResponse) => {
-    const id = req.params.id
-    const response = await getById("tarjeta", "encuentro", parseInt(id))
-    res.send(response)
+    try {
+        const id = req.params.id
+        const response = await getById("tarjeta", "encuentro", parseInt(id))
+        res.send(response)
+    } catch (error) {
+        if(error instanceof Error) res.status(404).send(error.message)
+        else res.status(404).send("Error")
+    }
+
 });
 //Obtener todos los goles
 partido_routes.get("/gol", async (req: IRequest, res: IResponse) => {
-    const response = await getAll("gol")
-    res.send(response)
+    try {
+        const response = await getAll("gol")
+        res.send(response)
+    } catch (error) {
+        if(error instanceof Error) res.status(404).send(error.message)
+        else res.status(404).send("Error")
+    }
+
 });
 //Obtener todas las tarjetas
 partido_routes.get("/tarjeta", async (req: IRequest, res: IResponse) => {
-    const response = await getAll("tarjeta")
-    res.send(response)
+    try {
+        const response = await getAll("tarjeta")
+        res.send(response)
+    } catch (error) {
+        if(error instanceof Error) res.status(404).send(error.message)
+        else res.status(404).send("Error")
+    }
+
 });
 //Obtener un gol por id
 partido_routes.get("/gol/:id", async (req: IRequest, res: IResponse) => {
-    const id = req.params.id
-    const response = await getById( "gol", "goldid",parseInt(id))
-    res.send(response)
+    try {
+        const id = req.params.id
+        const response = await getById( "gol", "goldid",parseInt(id))
+        res.send(response)
+    } catch (error) {
+        if(error instanceof Error) res.status(404).send(error.message)
+        else res.status(404).send("Error")
+    }
+
 });
 //Obtener una tarjeta por id
 partido_routes.get("/tarjeta/:id", async (req: IRequest, res: IResponse) => {
-    const id = req.params.id
-    const response = await getById( "tarjeta", "tarjetaid",parseInt(id))
-    res.send(response)
+    try {
+        const id = req.params.id
+        const response = await getById( "tarjeta", "tarjetaid",parseInt(id))
+        res.send(response)
+    } catch (error) {
+        if(error instanceof Error) res.status(404).send(error.message)
+        else res.status(404).send("Error")
+    }
+
 });
 //--------------------------ELIMINACION--------------------------//
 partido_routes.delete("/gol/:id", async (req: IRequest, res: IResponse) => {
-    const id = req.params.id
-    await deleteQueryById("gol", "goldid",parseInt(id))
-    res.send("Gol eliminado.")
+    try {
+        const id = req.params.id
+        await deleteQueryById("gol", "goldid",parseInt(id))
+        res.send("Gol eliminado.")
+    } catch (error) {
+        if(error instanceof Error) res.status(404).send(error.message)
+        else res.status(404).send("Error")
+    }
+
 });
 partido_routes.delete("/tarjeta/:id", async (req: IRequest, res: IResponse) => {
-    const id = req.params.id
-    await deleteQueryById("tarjeta", "tarjetaid",parseInt(id))
-    res.send("Tarjeta eliminada.")
+    try {
+        const id = req.params.id
+        await deleteQueryById("tarjeta", "tarjetaid",parseInt(id))
+        res.send("Tarjeta eliminada.")  
+    } catch (error) {
+        if(error instanceof Error) res.status(404).send(error.message)
+        else res.status(404).send("Error")
+    }
+
 });
--- src/Routes/torneo_routes.ts
+++ src/Routes/torneo_routes.ts
@@ -15,15 +15,27 @@ import divisionChecker from "./Utils/divisionCheck";
 //Rutas
 //Crear Torneo
 torneo_routes.post("/torneo", async (req: IRequest, res: IResponse) => {
-    const {nombre, periodo_inicio, periodo_inscripcion, division, categoria} = req.body
-    await postTorneo(nombre, periodo_inscripcion, periodo_inicio, divisionChecker(division), categoria)
-    res.send("Torneo y Fixture creado.")
+    try {
+        const {nombre, periodo_inicio, periodo_inscripcion, division, categoria} = req.body
+        await postTorneo(nombre, periodo_inscripcion, periodo_inicio, divisionChecker(division), categoria)
+        res.send("Torneo y Fixture creado.")
+    } catch (error) {
+        if(error instanceof Error) res.status(404).send(error.message)
+        else res.status(404).send("Error")
+    }
+
 });
 //Crear Fixture
 torneo_routes.post("/fixture", async (req: IRequest, res: IResponse) => {
-    const {fecha_inicio, division, categoria} = req.body
-    await postFixuture(fecha_inicio, divisionChecker(division), categoria, false)
-    res.send("Fixture creada.")
+    try {
+        const {fecha_inicio, division, categoria} = req.body
+        await postFixuture(fecha_inicio, divisionChecker(division), categoria, false)
+        res.send("Fixture creada.")
+    } catch (error) {
+        if(error instanceof Error) res.status(404).send(error.message)
+        else res.status(404).send("Error")
+    }
+
 });
 //Crear torneo Completo
 torneo_routes.post("/torneo_completo", async (req: IRequest, res: IResponse) => {
@@ -40,34 +52,70 @@ torneo_routes.post("/torneo_completo", async (req: IRequest, res: IResponse) => 
 //--------------------------GETTER--------------------------//
 //Trae todos los equipos de un torneo
 torneo_routes.get("/equipos/:id", async (req: IRequest, res: IResponse) => {
-    const id = req.params.id
-    const response = await getEquiposTorneo(parseInt(id))
-    res.send(response)
+    try {
+        const id = req.params.id
+        const response = await getEquiposTorneo(parseInt(id))
+        res.send(response)
+    } catch (error) {
+        if(error instanceof Error) res.status(404).send(error.message)
+        else res.status(404).send("Error")
+    }
+
 })
 //Trae los torneos completos
 torneo_routes.get("/torneo_completo", async (req: IRequest, res: IResponse) => {
-    const response = await getTorneoCompleto()
-    res.send(response)
+    try {
+        const response = await getTorneoCompleto()
+        res.send(response)
+    } catch (error) {
+        if(error instanceof Error) res.status(404).send(error.message)
+        else res.status(404).send("Error")
+    }
+
 })
 //Trae todos los torneos
 torneo_routes.get("/torneo", async (req: IRequest, res: IResponse) => {
-    const response = await getAll("torneo")
-    res.send(response)
+    try {
+        const response = await getAll("torneo")
+        res.send(response)
+    } catch (error) {
+        if(error instanceof Error) res.status(404).send(error.message)
+        else res.status(404).send("Error")
+    }
+
 })
 //Trae todas las fixtures
 torneo_routes.get("/fixture", async (req: IRequest, res: IResponse) => {
-    const response = await getAll("fixture")
-    res.send(response)
+    try {
+        const response = await getAll("fixture")
+        res.send(response)
+    } catch (error) {
+        if(error instanceof Error) res.status(404).send(error.message)
+        else res.status(404).send("Error")
+    }
+
 })
 //Trae un torneo especifico
 torneo_routes.get("/torneo/:id", async (req: IRequest, res: IResponse) => {
-    const id = req.params.id
-    const response = await getById("torneo", "torneoid", parseInt(id))
-    res.send(response)
+    try {
+        const id = req.params.id
+        const response = await getById("torneo", "torneoid", parseInt(id))
+        res.send(response)
+    } catch (error) {
+        if(error instanceof Error) res.status(404).send(error.message)
+        else res.status(404).send("Error")
+    }
+
 })
 //Trae una fixture especifica
 torneo_routes.get("/fixture/:id", async (req: IRequest, res: IResponse) => {
-    const id = req.params.id
-    const response = await getById("fixture", "fixtureid", parseInt(id))
-    res.send(response)
+    try {
+        const id = req.params.id
+        const response = await getById("fixture", "fixtureid", parseInt(id))
+        res.send(response)
+    } catch (error) {
+        if(error instanceof Error) res.status(404).send(error.message)
+        else res.status(404).send("Error")
+    }
+
 })
